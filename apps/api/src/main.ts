d84c9d5e71679e180cc3972cdbc86c3fb48304c7
Archy <61071062+archyntabona@users.noreply.github.com>
2020-08-20 22:34:38 -0400

    refactor to have one pet collection

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,54 +2,32 @@ import * as express from 'express';
 
 const app = express();
 
-const dogs = [
-  { id: '1' , name: 'Rufus'},
-  { id: '2' , name: 'Max'},
-  { id: '3' , name: 'Fido'},
-  { id: '4' , name: 'Barkley'},
-  { id: '5' , name: 'Clifford'},
-];
-
-const turtles = [
-  { id: '1' , name: 'Donatello'},
-  { id: '2' , name: 'Raphael'},
-  { id: '3' , name: 'Michelangelo'},
-  { id: '4' , name: 'Donatello'},
-  { id: '5' , name: 'Ricky Tabby'},
-];
-
-const cats = [
-  { id: '1' , name: 'Snowball'},
-  { id: '2' , name: 'Mr. Pickles'},
-  { id: '3' , name: 'Jennifurr'},
+const pets = [
+  { id: '1' , type: 'dog', name: 'Rufus', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '2' , type: 'dog', name: 'Max', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '3' , type: 'dog', name: 'Fido', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '4' , type: 'dog', name: 'Barkley', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '5' , type: 'dog', name: 'Clifford', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '6' , type: 'turte', name: 'Donatello', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '7' , type: 'turte', name: 'Raphael', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '8' , type: 'turte', name: 'Michelangelo', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '9' , type: 'turte', name: 'Donatello', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '10' , type: 'turte', name: 'Ricky Tabby', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '11' , type: 'cat', name: 'Snowball', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '12' , type: 'cat', name: 'Mr. Pickles', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
+  { id: '13' , type: 'cat', name: 'Jennifurr', age: '', favoriteToy: '', preferedPark: '', profilePicture: ''},
 ];
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-app.get('/api/dog/:id', (req, res) => {
-  res.send(dogs[0]);
+app.get('/api/pet/:id', (req, res) => {
+  res.send(pets.filter(pet => pet.id === req.params['id']));
 });
 
-app.get('/api/dogs', (req, res) => {
-  res.send(dogs);
-});
-
-app.get('/api/cat/:id', (req, res) => {
-  res.send(this.cats[0]);
-});
-
-app.get('/api/cats', (req, res) => {
-  res.send(this.cats);
-});
-
-app.get('/api/turtle/:id', (req, res) => {
-  res.send(turtles[0]);
-});
-
-app.get('/api/turtles', (req, res) => {
-  res.send(this.turtles);
+app.get('/api/pets', (req, res) => {
+  res.send(pets);
 });
 
 const port = process.env.port || 3333;
